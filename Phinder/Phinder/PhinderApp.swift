f19a522a672a7bd3ffa0c03cc59c6bff742a0be5
Sebastian Hufeld <sebastian.hufeld@gmail.com>
2025-04-22 12:54:01 +0200

    Added XCode Project & Images for Readme

---
+++ Phinder/Phinder/PhinderApp.swift
@@ -0,0 +1,18 @@
+//
+//  PhinderApp.swift
+//  Phinder
+//
+//  Created by Sebastian Hufeld on 22.04.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PhinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
